58412716853aecd76d9c49461bd13a76dd41b605
ksj2083 <ksj2083@naver.com>
2023-05-17 09:37:12 +0900

    di 생성자 주입 - autowired 생략 가능

--- src/main/java/com/mycatgib/springstudy/di/basic2/BookStore.java
+++ src/main/java/com/mycatgib/springstudy/di/basic2/BookStore.java
@@ -7,23 +7,22 @@ import org.springframework.stereotype.Component;
 @Component(value = "bookStore")
 public class BookStore {
 
-	@Autowired
-	@Qualifier(value = "qv3")
-	private Book book;	
-	public BookStore() {
-		super();
-	}
+//	@Autowired
+//	@Qualifier(value = "qv3")
+	private final Book book;
+
+
+//	@Autowired
 	public BookStore(Book book) {
-		super();
 		this.book = book;
 	}
 	public Book getBook() {
 		return book;
 	}
 
-	public void setBook(Book book) {
-		this.book = book;
-	}
+//	public void setBook(Book book) {
+//		this.book = book;
+//	}
 	@Override
 	public String toString() {
 		return "BookStore [book=" + book + "]";
